593d898dd7ed76c9e710ec629af88048d614155d
falconer67 <141686355+falconer67@users.noreply.github.com>
2024-09-14 18:34:33 +0330

    Update keys.js

--- 1/keys/keys.js
+++ 1/keys/keys.js
@@ -80,6 +80,13 @@ const games = {
         promoId: 'bc72d3b9-8e91-4884-9c33-f72482f0db37',
         interval: 61,
         eventCount: 1,
+    },
+    12:{
+       name: 'Hide ball',
+       appToken: '4bf4966c-4d22-439b-8ff2-dc5ebca1a600',
+       promoId: '4bf4966c-4d22-439b-8ff2-dc5ebca1a600',
+       interval: 61,
+        eventCount: 1,
     }
 };
 //const ln = document.getElementById('ln');

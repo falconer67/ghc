2be53724e0eafa399a9a8e862ce64666c485976b
falconer67 <141686355+falconer67@users.noreply.github.com>
2024-09-23 20:23:48 +0330

    Delete nuxt directory

--- nuxt/Preloader.vue.DBUplQo8.js
+++
@@ -1,1 +0,0 @@
-import{d as c,o as r,c as t,a as s,u as e,a6 as o}from"./entry.D18hrNzw.js";const _={class:"preloader"},a=["src"],n=["src"],l=["src"],u=c({__name:"Preloader",setup(i){return(d,m)=>(r(),t("div",_,[s("img",{src:e(o)},null,8,a),s("img",{src:e(o)},null,8,n),s("img",{src:e(o)},null,8,l)]))}});export{u as _};
